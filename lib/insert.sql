30c1b9088e3ef20f18faab56c643b481067ba084
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-19 15:58:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frumpy", 1, "male", "brown", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frumpy", 1, "male", "purple", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 2, "male", "brown", "hungry", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Simon", 3, "male", "spotted", "n/a", "n");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Garfunkel", 4, "male", "striped", "singing", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("BB", 5, "female", "black", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Penelope", 6, "female", "pink", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mrs. Chocolate", 5, "female", "brown", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "blue", "sleepy", "y");
