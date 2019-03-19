734d8cd4a7168019930a10bc7bd5246655da807e
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-19 15:58:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frumpy
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 2, "male", "brown", "hungry", "y");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Simon", 3, "male", "spotted", "n/a", "n");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Garfunkel", 4, "male", "striped", "singing", "y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("BB", 5, "female", "black", "sleepy", "y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Penelope", 6, "female", "pink", "sleepy", "y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mrs. Chocolate", 5, "female", "brown", "sleepy", "y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "blue", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("BB", 5, "female", "black", "rowdy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Penelope", 6, "female", "pink", "relaxed", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mrs. Chocolate", 5, "female", "brown", "happy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "female", "blue", "stretching", "y");
