7555ef0472d74eb4d7f57eecc415ec5ac186d87f
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-19 15:59:02 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Frumpy", 1, "male", "purple", "sleepy", "y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 1, "male", "purple", "sleepy", "y");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 2, "male", "brown", "hungry", "y");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Simon", 3, "male", "spotted", "n/a", "n");
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Garfunkel", 4, "male", "striped", "singing", "y");
